2f1acbb264d69efa2cca327ff8b7ee38c0ed480d
Mohamedwaheed221 <panther221b@gmail.com>
2023-06-15 07:27:47 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry print
+ *
+ * Description : A C  program that print a line using printf function
+ *
+ * Return:Always  0 (Success)
+*/
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
