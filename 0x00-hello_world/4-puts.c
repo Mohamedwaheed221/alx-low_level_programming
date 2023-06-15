962147b081e595fa1a89490249b8babc3c37ebf0
Mohamedwaheed221 <panther221b@gmail.com>
2023-06-15 05:40:22 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#!/bin/bash
 #include <stdio.h>
-int main()
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle \n");
-return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
