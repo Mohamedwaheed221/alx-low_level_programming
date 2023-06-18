e605d2cb15f81fa3d5714ca87d7cd6d585e657ed
Mohamedwaheed221 <panther221b@gmail.com>
2023-06-18 10:03:46 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,6 @@ int main(void)
 	char letter;
 	for (letter = 'a' ; letter <= 'z' ; letter++)
 		putchar(letter);
-	putchar('\n')
 		for (letter = 'A' ; letter <= 'Z' ; letter++)
 			putchar(letter);
 	putchar('\n')
