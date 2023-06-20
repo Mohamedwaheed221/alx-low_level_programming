2ef069a28ebb234e70be4fea486f8482f3443d10
Mohamedwaheed221 <panther221b@gmail.com>
2023-06-20 10:15:03 -0700

    0

---
+++ C.learneing/if_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n=4;
+  if (n != 4)
+    printf("n is not 4");
+
+  else
+    printf ("n is 4");
+    return (0);
+}
---
+++ C.learneing/whileloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int i =100;
+	while(i != 0)
+	{
+		printf("good bay");
+	}
+	return (0);
+}
+
