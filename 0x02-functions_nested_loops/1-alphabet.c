aaf011ead9b1f3f182f48ffd556ea4b280af4651
Mohamedwaheed221 <panther221b@gmail.com>
2023-06-21 01:23:40 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
  * print_alphabet - function to print abc
  *
  * Return: Always 0
  */
+
 int main(voin)
 {
 	char c;
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c)
 	}
