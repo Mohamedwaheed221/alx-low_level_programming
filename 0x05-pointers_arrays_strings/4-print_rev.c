670c23afb8d5facbb5e1927d410774ae6496b17f
Mohamedwaheed221 <panther221b@gmail.com>
2023-06-27 13:45:55 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 	int length;
 
-	for (length = 0; s[length] != '\0'; lengh++)
+	for (length = 0; s[length] != '\0'; length++)
 	{
 	}
 
-	for (i = length - 1; i > = 0; i--)
+	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
