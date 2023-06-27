13b6d21b4ff0a0ccebec6990af789ae914ced0fe
Mohamedwaheed221 <panther221b@gmail.com>
2023-06-27 12:59:06 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * main - mack function pointer
- *
- * Return: always 0 (sucsess)
-*/
+ * reset_to_98 - updates the value *n points to.
+ * @n: the number to modify.
+ */
 
 void reset_to_98(int *n)
 {
