907d235a92555b14751162dc2cad87a93ec8b4c0
Mohamedwaheed221 <panther221b@gmail.com>
2023-07-02 04:07:06 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,24 @@
 #include "main.h"
 /**
- * more_numbers - a function that prints 10 times the numbers
- * Bwave ICT file
- * Return: void.
- */
-
-
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
+*/
 void more_numbers(void)
 {
-	int i;
-	char c;
+int number1, number2;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (c = '0'; c <= '14'; c++)
-			_putchar(c);
-	}
-	_putchar('\n');
+for (number1 = 0; number1 < 10; number1++)
+{
+for (number2 = 0; number2 <= 14; number2++)
+
+{
+if (number2 > 9)
+{
+_putchar((number2 / 10) + '0');
+}
+_putchar((number2 % 10) + '0');
+}
+_putchar(10);
+}
 }
