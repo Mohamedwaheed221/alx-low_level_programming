0a9acdbb0f95a50176b724281fe62ca11741e83f
Mohamedwaheed221 <panther221b@gmail.com>
2023-07-02 03:24:07 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '4')
+			break;
+		else if (c == '2')
+			break;
+		else if
+			_putchar(c);
+	}
+	_putchar('\n');
+}
