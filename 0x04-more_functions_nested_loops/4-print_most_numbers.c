7ca9305803f048fb939d3aee267641558c634a74
Mohamedwaheed221 <panther221b@gmail.com>
2023-07-02 03:26:06 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,12 +12,8 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (c == '4')
-			break;
-		else if (c == '2')
-			break;
-		else if
-			_putchar(c);
+		if (!(c == '2' || c == '4'))
+			_putchar(c)
 	}
 	_putchar('\n');
 }
