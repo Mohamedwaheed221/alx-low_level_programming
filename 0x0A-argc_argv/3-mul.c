231e71354ca23529abc0c92db5dbd1c4ae59bb26
Mohamedwaheed221 <panther221b@gmail.com>
2023-07-16 04:28:10 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,15 +12,17 @@
 int main(int argc, char *argv[])
 {
 	if (argc < 3)
-		printf("%s\n", Error);
-	else
 	{
+		printf("%s\n", Error);
+		return (1);
+	}
+
 		int x, y, sum = 0;
 
 		x = atoi(argv[1]);
 		y = atio(argv[2]);
 		sum = x * y;
 		printf("%d\n", sum);
-	}
-	return (0);
+
+		return (0);
 }
