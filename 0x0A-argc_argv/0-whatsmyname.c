df1ae66482ce88221c92f6c100298d66540a46d9
Mohamedwaheed221 <panther221b@gmail.com>
2023-07-16 03:46:43 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,10 @@
  * Return: always 0 (succses)
  */
 
-void  main(int argc; char *argv[])
+int main(int argc; char **argv[])
 {
 	(void)argc;
-	printf("%s\n", argv[0]);
 
+	printf("%s\n", argv[0]);
 	return (0);
 }
