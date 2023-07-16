e2c656633162cd6ce16521d4f42e1fc88e1883f7
Mohamedwaheed221 <panther221b@gmail.com>
2023-07-16 03:42:08 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argv: argument vector.
+ * Return: always 0 (succses)
+ */
+
+void  main(int argc; char *argv[])
+{
+	printf("my loc is %s", argv[0]);
+
+	return (0);
+}
