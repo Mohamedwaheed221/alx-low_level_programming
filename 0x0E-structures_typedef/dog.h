cfcebaab5bf6e0ad66d08ff05dc06fce843dcb00
Mohamedwaheed221 <panther221b@gmail.com>
2023-07-23 06:02:16 -0700

    d

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: just a long dog struct in a big kitty worled.
+ */
+
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+};
+
+#endif
