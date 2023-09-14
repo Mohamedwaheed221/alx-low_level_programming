723ced28436ee3e8971c9b89affaf31078f374b5
Mohamedwaheed221 <panther221b@gmail.com>
2023-09-13 22:40:02 -0700

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	if(size <= 0 || array == NULL || cmp == NULL)
-		return(-1);
 
-	for(i = 0; i < size; i++)
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == 1)
-			return(i);
+			return (i);
 	}
-	return(-1);
+	return (-1);
 }
