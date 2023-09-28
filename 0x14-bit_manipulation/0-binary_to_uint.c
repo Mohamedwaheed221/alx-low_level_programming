250a5b904b154af94235410579e85a7da05b4be4
Mohamedwaheed221 <panther221b@gmail.com>
2023-09-28 06:50:09 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int val = 0;
-	int i = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	while (b[i] == '0' || b[i] == '1')
+
+	for (i = 0; b[i]; i++)
 	{
-		val <<= 1;
-		val += b[i] - '0';
-		i++;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (val);
+	return (dec_val);
 }
