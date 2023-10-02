6c4f5a6322429223cc6db526a004566d70a7ebcf
Mohamedwaheed221 <panther221b@gmail.com>
2023-10-02 08:40:32 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,26 +1,37 @@
 #include "main.h"
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read.
- * @letters: the number of bytes to read.
- *
- * Return: numbers of bytes that read and print.
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int *ptr;
-	ssize_t bytes;
-	char buf[READ_BUF_SIZE * 8];
-
-	if (!filename || !letters)
+	int fd;
+	int i, y;
+	char *buf;
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
-	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILEND, &buf[0], bytes);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[i] = '\0';
 	close(fd);
-	return (bytes);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
 }
